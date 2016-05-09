25f7f7b0cdfa503df32444d7dc87b864a256a7eb
sylvaintauxe <Sylvain@10.0.2.15>
2016-05-09 16:08:54 +0200

    Affichage des informations des utilisateurs (Annonceur et transporteur) sur un page. Ajout la possibilité de modifier certaines de leurs informations (Mot de passe, adresse 1&2 et la Société).
    Création de la requête SQL pour l'Update. Modification du fichier
    "check_user_info" en conséquence, pour gérer l'update.

--- pages/infoUser.php
+++ pages/infoUser.php
@@ -9,7 +9,7 @@
  *
  * Author		Sylvain Tauxe
  * Creation		20160418
- * modification 20160424
+ * modification 20160509
  *
  * Project		teemw
  *
@@ -28,15 +28,72 @@ if (isset($_SESSION['user'])){
 	$user = unserialize($_SESSION['user']);
 	
 	//var_dump($user);
-	echo $user->getTitle().'</br>';
-	echo $user->getFirstname().'</br>';
-	echo $user->getLastname().'</br>';
-	echo $user->getAddress1().'  ';
+	if($user->getRole()==3){
+		echo 'Society : '.$user->getSociety().'</br>';
+	}
+	echo 'Titre : '.$user->getTitle().'</br>';
+	echo 'Firstname : '.$user->getFirstname().'</br>';
+	echo 'Lastname : '.$user->getLastname().'</br>';
+	echo 'Adress 1&2 : '.$user->getAddress1().',  ';
 	echo $user->getAddress2().'</br>';
 	echo 'role : '.$user->getRole().'</br>';
-	echo $user->getEmail().'</br>';
+	echo 'Email : '.$user->getEmail().'</br>';
 	
 ?>
+<br>
+Update infos :<br>
+<?php if(isset($_SESSION['rank'])){
+	if ($_SESSION['rank'] == 30){
+		echo $_SESSION['msg'];
+	}
+}
+?>
+<br>
+<form method="post" action="../business/check_info_user.php">
+<table>
+	<tr>
+		<td>
+		Change password :
+		</td>
+		<td>
+		<input type="password" name="updatePassword" value="">
+		</td>
+	</tr>
+
+	<tr>
+		<td>
+		New adress1 :
+		</td>
+		<td>
+		<input type="text" name="updateAdress1" value="">
+		</td>
+	</tr>
+	<tr>
+		<td>
+		New adress2 :
+		</td>
+		<td>
+		<input type="text" name="updateAdress2" value="">
+		</td>
+	</tr>
+	<?php 
+		if($user->getRole()==3){
+	?>
+	<tr>
+		<td>
+		New Society name :
+		</td>
+		<td>
+		<input type="text" name="updateSociety" value="">
+		</td>
+	</tr>
+	<?php }?>
+	<tr>
+		<td colspan="2" align="right"> <input type="submit" name="action" value="Update your info"></td>			
+	</tr>
+</table>
+
+</form>
 <a href="../business/check_info_user.php?action=logout">Logout</a>
 <?php 
 }
