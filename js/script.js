4d5a5a3c5f3cf18f35f12aac06758831a28a484c
anthonycheseaux <anthony.cheseaux@students.hevs.ch>
2016-05-12 15:42:56 +0200

    smooth scroll

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function() {
+	//smooth scroll
     $('a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
