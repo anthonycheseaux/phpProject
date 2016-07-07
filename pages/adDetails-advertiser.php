f4b43ecff0b4b8eed5dc84a7ff8c7649d26c9e24
anthonycheseaux <anthony.cheseaux@students.hevs.ch>
2016-07-07 19:34:18 +0200

    design

---
+++ pages/adDetails-advertiser.php
@@ -0,0 +1,129 @@
+<?php
+/************************************************************\
+ *
+ * File			adDetails-advertiser.php
+ *
+ * Language		PHP
+ *
+ * Author		Anthony Cheseaux
+ * Creation		20160705
+ *
+ * Project		teemw
+ * Package		include
+ *
+ * Description	common header
+ *
+ \************************************************************/
+require_once ('../ressources/templates/header.php');
+require_once ('../tools/database/mysqladmanager.php');
+
+if(isset($_SESSION['rank'])) {
+	$rank = $_SESSION['rank'];
+	$msg = $_SESSION['msg'];
+	echo '<script type="text/javascript">window.alert("' . $msg . '");</script>';
+	unset($_SESSION['rank']);
+}
+ 
+?>
+
+<body>
+
+	<div class="container">
+	<div style="text-align: center">
+		<h2 style="text-align: center;"><?php echo _AD_DETAILS?></h2>
+		<br>
+		<FORM><INPUT Type="button" VALUE="X" onClick="window.location.href='./adlist-advertiser.php';" class="buttonCloseAdDetails"></FORM>
+<?php
+
+$ad_id = $_GET ['id'];
+
+// $namedb = 'grp1';
+// $db = 'localhost';
+// $user = 'root';
+// $pwd = '';
+// $error = 'Could not connect';
+
+// if (! mysql_connect ( $db, $user, $pwd ) || ! mysql_select_db ( $namedb )) {
+// 	die ( $error );
+// }
+
+
+$adManager = new MySqlAdManager ();
+
+if ($ad = $adManager->getAd ( $ad_id )) {
+	if (empty ( $ad )) {
+		die ( $error );
+	}
+}
+
+
+function chooseCategory($var) {
+	
+}
+?>
+
+<table class="table-striped" id="ad-details">
+			<tr>
+				<td><?php echo _AD_TITLE ?></td>
+				<td><?php echo $ad->getTitle () ?></td>
+			</tr>
+			
+			<tr>
+				<td><?php echo _AD_CATEGORY ?></td>
+				<td><?php $c = $ad->getCategory(); echo constant($c);  ?></td>
+			</tr>
+			
+			<tr>
+				<td><?php echo _AD_DESCRIPTION ?></td>
+				<td><?php echo $ad->getDescription() ?></td>
+			</tr>
+			
+			<tr>
+				<td><?php echo _AD_DEPARTURE_CI ?></td>
+				<td><?php echo $ad->getDeparture_city () ?></td>
+			</tr>
+			
+			<tr>
+				<td><?php echo _AD_DATE_BEG ?></td>
+				<td><?php echo $ad->getDate_beginning() ?></td>
+			</tr>
+
+			<tr>
+				<td><?php echo _AD_DESTINATION_CI ?></td>
+				<td><?php echo $ad->getDestination_city () ?></td>
+			</tr>
+			
+			<tr>
+				<td><?php echo _AD_DATE_END ?></td>
+				<td><?php echo $ad->getDate_end() ?></td>
+			</tr>
+			
+			<tr>
+				<td><?php echo _AD_TOTAL_WEIGHT ?></td>
+				<td><?php echo $ad->getTotal_weight() ?></td>
+			</tr>
+			
+			<tr>
+				<td><?php echo _AD_TOTAL_VOLUME ?></td>
+				<td><?php echo $ad->getTotal_volume() ?></td>
+			</tr>
+			
+			<tr>
+				<td><?php echo _AD_OBJECT_NUM ?></td>
+				<td><?php 
+				
+				if($ad->getObjects_number() == '') {
+					echo("-");
+				}
+				else {
+					echo $ad->getObjects_number();
+				}
+				 
+				
+				?></td>
+			</tr>
+</table>
+<br>
+	</div>
+	</div>
+</body>
---
+++ ressources/templates/nav-backoffice-advertiser.php
@@ -0,0 +1,56 @@
+<?php
+/************************************************************\
+ *
+ * File			navbar-backoffice.php
+ *
+ * Language		PHP
+ *
+ * Author		Anthony Cheseaux
+ * Creation		20160707
+ *
+ * Project		teemw
+ * Package		include
+ * 
+ * Description	common header
+ * 
+ \************************************************************/
+
+require_once ('../ressources/templates/header.php');
+?>
+<body>
+	<nav class="navbar navbar-full navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">BACKOFFICE TEEMW</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="./infoAdvertiser.php"><?php echo _INFO_USER?></a></li>
+				<li><a href="./adlist-customer.php"><?php echo _AD_LIST?></a></li>
+			</ul>
+<ul class="nav navbar-nav navbar-right">
+					
+						
+					<!-- LOGOUT -->	
+					<li><a href="../business/check_info_user.php?action=logout"><span
+						class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+					
+				</ul>
+
+
+				<ul class="nav navbar-nav navbar-right">
+						
+					<!-- LANGUAGES -->
+					<li class="dropdown pull-right"><a href="#" class="dropdown-toggle"
+						data-toggle="dropdown" role="button" aria-haspopup="true"
+						aria-expanded="false"><?php echo _LANGUAGE?><span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="?lang=fr"><img src="image/frenchFlag.png"></a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="?lang=en"><img src="image/britishFlag.png"></a></li>
+						</ul>
+					</li>
+				</ul>
+		</div>
+	</nav>
+	
+	<div style="width: 100%; height: 100px"></div>
