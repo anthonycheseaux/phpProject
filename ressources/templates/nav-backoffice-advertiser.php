152c73040617346f50b3cb6cfdf1a8d86124ff65
anthonycheseaux <anthony.cheseaux@students.hevs.ch>
2016-07-08 08:20:31 +0200

    navbar

--- ressources/templates/nav-backoffice-advertiser.php
+++ ressources/templates/nav-backoffice-advertiser.php
@@ -46,7 +46,7 @@ require_once ('../ressources/templates/header.php');
 			<div id="myNavbar" class="navbar-collapse collapse">
 				<ul class="nav navbar-nav">
 					<li><a href="./infoAdvertiser.php"><?php echo _INFO_USER?></a></li>
-					<li><a href="./adlist.php"><?php echo _AD_LIST?></a></li>
+					<li><a href="./adlist-advertiser.php"><?php echo _AD_LIST?></a></li>
 					<li><a href="./inputAd.php"><?php echo _AD_CREATE?></a></li>
 				</ul>
 				
